e53938ad2cc19cb26605e4869713b95e615fa594
Metin Ljapo <metttin@gmail.com>
2016-10-31 08:49:27 +0100

    Desktop top bar

--- Frontend/eshop/src/App.js
+++ Frontend/eshop/src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+      		<nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				<div className="container">
+					<div className="navbar-header">
+						<a className="navbar-brand" href="index.html">Start Bootstrap</a>
+					</div>
+					<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul className="nav navbar-nav navbar-right">
+							<li>
+								<a href="#">About</a>
+							</li>
+							<li>
+								<a href="#">Services</a>
+							</li>
+							<li>
+								<a href="#">Contact</a>
+							</li>
+							<li>
+								<a href="#">Other Pages</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+      	</div>
     );
   }
 }
